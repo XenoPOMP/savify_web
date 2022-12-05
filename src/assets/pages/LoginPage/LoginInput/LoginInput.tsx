09c154133aaa3f4dc86712116a8eed117112896a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-12-05 21:05:07 +0300

    create work with states in input

--- src/assets/pages/LoginPage/LoginInput/LoginInput.tsx
+++ src/assets/pages/LoginPage/LoginInput/LoginInput.tsx
@@ -3,12 +3,17 @@ import cn from 'classnames';
 import styles from './LoginInput.module.scss';
 import { LoginInputProps } from './LoginInput.props';
 
-const LoginInput: FC<LoginInputProps> = ({ label, placeholder }) => {
+const LoginInput: FC<LoginInputProps> = ({ label, placeholder, useState }) => {
   return (
     <div className={cn(styles.input)}>
       <div className={cn(styles.container)}>
         <div className={cn(styles.label)}>{label}</div>
-        <input type={'text'} placeholder={placeholder} />
+        <input
+          type={'text'}
+          placeholder={placeholder}
+          value={useState.state}
+          onChange={(e) => useState.setState(e.target.value)}
+        />
       </div>
     </div>
   );
